f2d6bf496e308b65778e1373efd41b0ed018926e
Eric Smith <ericsmith.100@gmail.com>
2020-08-25 10:10:40 -0400

    Implement SEO

--- gatsby-theme-minimal/src/components/LayoutWoodbury.js
+++ gatsby-theme-minimal/src/components/LayoutWoodbury.js
@@ -1,6 +1,7 @@
 /** @jsx jsx */
 import { jsx, Box } from 'theme-ui';
 import React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
 
 import '../../../node_modules/animate.css';
 import './animate.css';
@@ -12,8 +13,52 @@ import Navigation from './Navigation';
 import SmoothHero from './SmoothHero';
 import Footer from './Footer';
 import SimpleHero from './SimpleHero';
+import SEO from './SEO'
 
 const Layout = ({ children, pageTitle, location }) => {
+
+  const data = useStaticQuery(graphql`
+    query metaQuery {
+      siteData(data: { city: { eq: "Woodbury" } }) {
+        data {
+          avatar {
+            imageBaseUrl
+            imagePrefix
+            isDefault
+          }
+          city
+          cover {
+            imageBaseUrl
+            imagePrefix
+            isDefault
+          }
+          desc
+          lastPricelistUpdate {
+            sec
+            usec
+          }
+          links {
+            facebook
+            instagram
+            twitter
+            website
+            youtube
+          }
+          loc
+          name
+          phone
+          slug
+          state
+          street
+          zip
+        }
+      }
+    }
+  `);
+
+  console.log('data: ', data.siteData.data)
+  const site = data.siteData.data
+
   const renderHero = () => (
     <>
       {pageTitle === 'home' ? (
@@ -25,12 +70,15 @@ const Layout = ({ children, pageTitle, location }) => {
   );
 
   return (
-    <Box as='main'>
-      <Navigation location="Woodbury" />
-      {pageTitle === 'about' || pageTitle === 'contact' ? '' : renderHero()}
-      {children}
-      <Footer location="Woodbury"></Footer>
-    </Box>
+    <>
+      <SEO siteMetaData={site} pageTitle={pageTitle} />
+      <Box as='main'>
+        <Navigation location='Woodbury' />
+        {pageTitle === 'about' || pageTitle === 'contact' ? '' : renderHero()}
+        {children}
+        <Footer location='Woodbury'></Footer>
+      </Box>
+    </>
   );
 };
 
