57c02340e8c928054b9dbe30e34f6fda4c6f9f9b
Eric Smith <ericsmith.100@gmail.com>
2020-09-01 12:22:40 -0400

    attempt without using reduce

--- gonation-theme-custom/src/components/HoursComponent.js
+++ gonation-theme-custom/src/components/HoursComponent.js
@@ -18,6 +18,7 @@ const HoursComponent = ({ id }) => {
     });
   }, []);
 
+  // returns an array of all hour labels associated with the business
   const getUniqueNames = hours => {
     const titles = Object.keys(hours).map(day => {
       const uniqueNames = hours[day].reduce((acc, cur) => {
@@ -27,17 +28,64 @@ const HoursComponent = ({ id }) => {
       }, []);
       return [...uniqueNames];
     });
+
     return titles[0];
   };
 
-  const buildHoursObj = labels => {};
+  // Takes an accumalitive hours object, and an hours label. Returns true if it already exists, else it returns false
+  const hoursExists = (hours, cur) =>
+    hours.filter(hour => hour.name === cur).length ? true : false;
+
+  const getTimeBlock = label => {
+    console.log('label: ', label, 'hours: ', hours);
+    const days = Object.keys(hours);
+    // loop through hours and filter just the ones with the same label
+
+    // we want to return an array of objects looking like:
+    // [
+    //  {days: [], time: ''}
+    // ]
+    //
+    const sameHours = days.map(day => {
+      return hours[day]
+        .filter(hour => hour.name === label)
+        .map(hour => {
+          if (hour.name === label) {
+            return {
+              days: [day],
+              hours: `${hour.open}-${hour.close}`,
+            };
+          }
+        })[0];
+    });
+
+    console.log('HoursComponent -> sameHours', sameHours);
+    sameHours.map(hour => console.log(hour));
+  };
+
+  // Takes an array of labels, and returns the new hours Array
+  const buildHoursObj = labels => {
+    console.log('OK, labels are: ', labels);
+    const hoursObj = labels.reduce((acc, cur, idx) => {
+      if (hoursExists(acc, cur)) {
+        // if hours is already in our new array, we check to see if
+      } else {
+        // Else, we create a new accumulator
+        console.log('else', hours);
+        acc.push({
+          name: cur,
+          timeBlock: getTimeBlock(cur),
+        });
+      }
+      return acc;
+    }, []);
+    console.log('HoursComponent -> hoursObj', hoursObj);
+  };
 
   useEffect(() => {
     if (hours) {
       const uniqueHours = getUniqueNames(hours);
       buildHoursObj(uniqueHours);
-
-      console.log('HoursComponent -> uniqueHours', uniqueHours);
     }
   }, [hours]);
 
