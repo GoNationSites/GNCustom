7df15a04dbc68dc1c88a51f7835af1328837cd15
Eric Smith <ericsmith.100@gmail.com>
2020-09-02 08:13:24 -0400

    Replace dummy text with real data

--- gatsby-theme-minimal/src/pages/woodbury/index.js
+++ gatsby-theme-minimal/src/pages/woodbury/index.js
@@ -1,27 +1,26 @@
 /** @jsx jsx */
-import { jsx, Box } from 'theme-ui'
+import { jsx, Box } from 'theme-ui';
 import PageTransition from 'gatsby-plugin-page-transitions';
 
 import React from 'react';
 
-import cloudinaryOptimize from '../../helpers/cloudinaryHelper'
+import cloudinaryOptimize from '../../helpers/cloudinaryHelper';
 import Layout from '../../components/LayoutWoodbury';
-import AboutText from '../../components/AboutText'
+import AboutText from '../../components/AboutText';
 import ContentShowcase from '../../components/ContentShowcase';
 import MenuShowcaseSlider from '../../components/MenuShowcaseSlider';
 import cloudinaryHelper from '../../helpers/cloudinaryHelper';
 
 const Woodbury = () => {
-
   const contentShowcaseData = [
     {
       img: cloudinaryHelper(
         'https://res.cloudinary.com/gonation/image/upload/v1598359600/sites/mix-prime/woodbury-content-1.jpg',
         1200
       ),
-      title: 'Title can go here',
+      title: 'Voted the best steakhouse for a reason',
       txt:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem quis, asperiores esse.',
+        'Taste a difference at mix prime with hand selected steaks, mouth watering sushi and craft cocktails. ',
       btn: {
         title: 'View Menu',
         link: 'woodbury/menu',
@@ -32,41 +31,40 @@ const Woodbury = () => {
         'https://res.cloudinary.com/gonation/image/upload/v1598359600/sites/mix-prime/woodbury-content-2.jpg',
         1200
       ),
-      title: 'Some other title here',
-      txt:
-        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi mollitia.',
+      title: 'Discover what’s special',
+      txt: 'Don’t miss out on our Sunday Brunch and bar specials.',
       btn: {
         title: 'Learn More',
-        link: 'woodbury/about',
+        link: 'woodbury/events',
       },
     },
   ];
 
-    return (
-      <PageTransition
-        defaultStyle={{
-          transition: 'left 500ms cubic-bezier(0.47, 0, 0.75, 0.72)',
-          left: '100%',
-          position: 'absolute',
-          width: '100%',
-        }}
-        transitionStyles={{
-          entering: { left: '0%' },
-          entered: { left: '0%' },
-          exiting: { left: '100%' },
-        }}
-        transitionTime={500}>
-        <Layout pageTitle='home'>
-          <Box sx={{ maxWidth: '730px', margin: 'auto' }}>
-            <AboutText location="Woodbury"></AboutText>
+  return (
+    <PageTransition
+      defaultStyle={{
+        transition: 'left 500ms cubic-bezier(0.47, 0, 0.75, 0.72)',
+        left: '100%',
+        position: 'absolute',
+        width: '100%',
+      }}
+      transitionStyles={{
+        entering: { left: '0%' },
+        entered: { left: '0%' },
+        exiting: { left: '100%' },
+      }}
+      transitionTime={500}>
+      <Layout pageTitle='home'>
+        <Box sx={{ maxWidth: '730px', margin: 'auto' }}>
+          <AboutText location='Woodbury'></AboutText>
 
-            <ContentShowcase  data={contentShowcaseData} />
-          </Box>
+          <ContentShowcase data={contentShowcaseData} />
+        </Box>
 
-          <MenuShowcaseSlider id='bzn-mmT_2ynbR4eGFehR2VEi8g' pl='1' />
-        </Layout>
-      </PageTransition>
-    );
-}
+        <MenuShowcaseSlider id='bzn-mmT_2ynbR4eGFehR2VEi8g' pl='1' />
+      </Layout>
+    </PageTransition>
+  );
+};
 
 export default Woodbury;
