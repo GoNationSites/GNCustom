2c4393f21ec7ff8e5039e83d0e439ba0668ae50a
Eric Smith <ericsmith.100@gmail.com>
2020-09-02 10:26:17 -0400

    Implement about changes

--- gatsby-theme-minimal/src/pages/danbury/about.js
+++ gatsby-theme-minimal/src/pages/danbury/about.js
@@ -1,8 +1,8 @@
 /** @jsx jsx */
 import { jsx, Box, Flex, Text, Image } from 'theme-ui';
 import React, { useEffect, useState } from 'react';
-import axios from 'axios'
-import jsonpAdapter from 'axios-jsonp'
+import axios from 'axios';
+import jsonpAdapter from 'axios-jsonp';
 
 import Layout from '../../components/Layout';
 import cloudinaryHelper from '../../helpers/cloudinaryHelper';
@@ -13,28 +13,26 @@ const About = () => {
     isLoading: true,
   });
 
-  
-    const id = 'bzn-vBPQfZmCTC2V2Y_BpE6SPw';
+  const id = 'bzn-vBPQfZmCTC2V2Y_BpE6SPw';
 
-    useEffect(() => {
-      axios({
-        url: `https://data.prod.gonation.com/profile/getname/?profile_id=${id}`,
-        adapter: jsonpAdapter,
-      }).then(res => {
-        setAboutData({
-          ...about,
-          aboutData: res.data,
-          isLoading: false,
-        });
+  useEffect(() => {
+    axios({
+      url: `https://data.prod.gonation.com/profile/getname/?profile_id=${id}`,
+      adapter: jsonpAdapter,
+    }).then(res => {
+      setAboutData({
+        ...about,
+        aboutData: res.data,
+        isLoading: false,
       });
-    }, []);
-  
+    });
+  }, []);
 
   return (
     <Layout pageTitle='about'>
       <Box
         sx={{
-          height: ['auto','100vh'],
+          height: ['auto', '100vh'],
           backgroundImage: `linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url(${cloudinaryHelper(
             'https://res.cloudinary.com/gonation/image/upload/v1598281036/sites/mix-prime/danbury-about-hero.jpg',
             2000
@@ -47,14 +45,21 @@ const About = () => {
             alignItems: 'center',
             justifyContent: 'center',
             height: '100%',
-            flexWrap: 'wrap'
+            flexWrap: 'wrap',
           }}>
-          <Box sx={{ width: ['100%', '44%', '55%'], textAlign: 'center', pt: [5, 0, 0], pb: [4,0] }}>
+          <Box
+            sx={{
+              width: ['100%', '44%', '55%'],
+              textAlign: 'center',
+              pt: [5, 0, 0],
+              pb: [4, 0],
+            }}>
             <Image
-            alt={'About Mix Prime'}
-            sx={{maxWidth: '400px', }}
+              alt={'About Mix Prime'}
+              sx={{ maxWidth: '400px' }}
               src={cloudinaryHelper(
-                'https://res.cloudinary.com/gonation/image/upload/v1598282284/sites/mix-prime/danbury-about-1.jpg', 900
+                'https://res.cloudinary.com/gonation/image/upload/v1598282284/sites/mix-prime/danbury-about-1.jpg',
+                900
               )}
             />
           </Box>
@@ -64,6 +69,20 @@ const About = () => {
               sx={{ fontSize: [2, 3], lineHeight: 2, color: 'white' }}>
               {about.isLoading ? '' : about.aboutData.desc}
             </Text>
+            <Flex sx={{ mt: [2, 3, 4] }}>
+              <Box sx={{ marginRight: [2, 3, 4] }}>
+                <Image
+                  src='https://res.cloudinary.com/gonation/image/upload/v1599056333/sites/mix-prime/award-1.png'
+                  alt='restaurant award'
+                />
+              </Box>
+              <Box>
+                <Image
+                  src='https://res.cloudinary.com/gonation/image/upload/v1599056333/sites/mix-prime/award-2.png'
+                  alt='restaurant award'
+                />
+              </Box>
+            </Flex>
           </Box>
         </Flex>
       </Box>
--- gatsby-theme-minimal/src/pages/woodbury/about.js
+++ gatsby-theme-minimal/src/pages/woodbury/about.js
@@ -1,8 +1,8 @@
 /** @jsx jsx */
 import { jsx, Box, Flex, Text, Image } from 'theme-ui';
 import React, { useEffect, useState } from 'react';
-import axios from 'axios'
-import jsonpAdapter from 'axios-jsonp'
+import axios from 'axios';
+import jsonpAdapter from 'axios-jsonp';
 
 import Layout from '../../components/LayoutWoodbury';
 import cloudinaryHelper from '../../helpers/cloudinaryHelper';
@@ -13,22 +13,20 @@ const About = () => {
     isLoading: true,
   });
 
-  
-    const id = 'bzn-mmT_2ynbR4eGFehR2VEi8g';
+  const id = 'bzn-mmT_2ynbR4eGFehR2VEi8g';
 
-    useEffect(() => {
-      axios({
-        url: `https://data.prod.gonation.com/profile/getname/?profile_id=${id}`,
-        adapter: jsonpAdapter,
-      }).then(res => {
-        setAboutData({
-          ...about,
-          aboutData: res.data,
-          isLoading: false,
-        });
+  useEffect(() => {
+    axios({
+      url: `https://data.prod.gonation.com/profile/getname/?profile_id=${id}`,
+      adapter: jsonpAdapter,
+    }).then(res => {
+      setAboutData({
+        ...about,
+        aboutData: res.data,
+        isLoading: false,
       });
-    }, []);
-  
+    });
+  }, []);
 
   return (
     <Layout pageTitle='about'>
@@ -57,7 +55,7 @@ const About = () => {
               pb: [4, 0],
             }}>
             <Image
-            alt="About Mix Prime"
+              alt='About Mix Prime'
               sx={{ maxWidth: '400px' }}
               src={cloudinaryHelper(
                 'https://res.cloudinary.com/gonation/image/upload/v1598359982/sites/mix-prime/woodbury-about-1.jpg',
@@ -71,6 +69,20 @@ const About = () => {
               sx={{ fontSize: [2, 3], lineHeight: 2, color: 'white' }}>
               {about.isLoading ? '' : about.aboutData.desc}
             </Text>
+            <Flex sx={{ mt: [2, 3, 4] }}>
+              <Box sx={{ marginRight: [2, 3, 4] }}>
+                <Image
+                  src='https://res.cloudinary.com/gonation/image/upload/v1599056333/sites/mix-prime/award-1.png'
+                  alt='restaurant award'
+                />
+              </Box>
+              <Box>
+                <Image
+                  src='https://res.cloudinary.com/gonation/image/upload/v1599056333/sites/mix-prime/award-2.png'
+                  alt='restaurant award'
+                />
+              </Box>
+            </Flex>
           </Box>
         </Flex>
       </Box>
