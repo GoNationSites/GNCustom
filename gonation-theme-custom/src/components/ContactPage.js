3b817eb57838f2e5622992cd19505cf20ff39ae1
esmith7196 <ericsmith.100@gmail.com>
2022-08-11 11:21:30 -0400

    [skip ci] Make rooster change

--- gonation-theme-custom/src/components/ContactPage.js
+++ gonation-theme-custom/src/components/ContactPage.js
@@ -11,27 +11,31 @@ import ContactForm from './ContactForm';
 import HoursComponent from './HoursComponent';
 
 const ContactPage = ({ data, location, id }) => {
+  console.log('location is: ', location);
   const { googleMapLink } = data;
+  const isNewtown = location.includes('newtown');
   return (
     <Box>
       <Box>
         <iframe
           src={googleMapLink}
-          width='100%'
-          height='350'
-          frameborder='0'
-          allowfullscreen=''
-          aria-hidden='false'
-          tabindex='0'></iframe>
+          width="100%"
+          height="350"
+          frameborder="0"
+          allowfullscreen=""
+          aria-hidden="false"
+          tabindex="0"
+        ></iframe>
       </Box>
       <Flex sx={{ flexWrap: 'wrap', alignItems: 'stretch' }}>
         <Box
           sx={{
-            flex: ['1 1 100%', '1 1 50%', '1'],
+            flex: ['1 1 100%', '1 1 50%', `${isNewtown} ? '1 1 50%' : '1'`],
             padding: 3,
             paddingLeft: 0,
             height: '100%',
-          }}>
+          }}
+        >
           <Box
             sx={{
               bg: 'primary',
@@ -39,14 +43,15 @@ const ContactPage = ({ data, location, id }) => {
               color: 'white',
               borderRadius: '5px',
               height: '100%',
-            }}>
-            <Text variant='heading' sx={{ fontSize: 4, mb: 3 }}>
+            }}
+          >
+            <Text variant="heading" sx={{ fontSize: 4, mb: 3 }}>
               Contact Details
             </Text>
-            <Text as='h4' variant='heading' sx={{ fontSize: 3 }}>
+            <Text as="h4" variant="heading" sx={{ fontSize: 3 }}>
               Address
             </Text>
-            <Text as='p' sx={{ mb: 3 }}>
+            <Text as="p" sx={{ mb: 3 }}>
               <ClickableAddress
                 data={data.data}
                 style={{
@@ -57,10 +62,10 @@ const ContactPage = ({ data, location, id }) => {
                 }}
               />
             </Text>
-            <Text as='h4' variant='heading' sx={{ fontSize: 3 }}>
+            <Text as="h4" variant="heading" sx={{ fontSize: 3 }}>
               Phone
             </Text>
-            <Text as='p' sx={{ mb: 3 }}>
+            <Text as="p" sx={{ mb: 3 }}>
               <ClickablePhone
                 tel={data.data.phone}
                 style={{
@@ -68,12 +73,13 @@ const ContactPage = ({ data, location, id }) => {
                   '&:hover': {
                     color: 'text',
                   },
-                }}></ClickablePhone>
+                }}
+              ></ClickablePhone>
             </Text>
-            <Text as='h4' variant='heading' sx={{ fontSize: 3 }}>
+            <Text as="h4" variant="heading" sx={{ fontSize: 3 }}>
               Social
             </Text>
-            <Text as='p' sx={{ mb: 3, maxWidth: '200px', marginRight: 'auto' }}>
+            <Text as="p" sx={{ mb: 3, maxWidth: '200px', marginRight: 'auto' }}>
               <SocialIcons
                 links={data.data.links}
                 slug={data.data.slug}
@@ -85,12 +91,13 @@ const ContactPage = ({ data, location, id }) => {
         </Box>
         <Box
           sx={{
-            flex: ['1 1 100%', '1 1 50%', '1'],
+            flex: ['1 1 100%', `1 1 50%`, `${isNewtown} ? '1 1 50%' : '1'`],
             padding: 3,
             height: '100%',
-          }}>
+          }}
+        >
           <Box sx={{ bg: 'white', padding: 3, borderRadius: '5px' }}>
-            <Text variant='heading' sx={{ fontSize: 4, mb: 3 }}>
+            <Text variant="heading" sx={{ fontSize: 4, mb: 3 }}>
               Get in touch
             </Text>
             <ContactForm location={location} />
@@ -102,20 +109,26 @@ const ContactPage = ({ data, location, id }) => {
             padding: 3,
             paddingRight: 0,
             height: '100%',
-          }}>
-          <Box
-            sx={{
-              bg: 'primary',
-              padding: 3,
-              color: 'white',
-              borderRadius: '5px',
-              height: '100%',
-            }}>
-            <Text variant='heading' sx={{ fontSize: 4, mb: 3 }}>
-              Hours
-            </Text>
-            <HoursComponent id={id} location={location} />
-          </Box>
+          }}
+        >
+          {!isNewtown ? (
+            <Box
+              sx={{
+                bg: 'primary',
+                padding: 3,
+                color: 'white',
+                borderRadius: '5px',
+                height: '100%',
+              }}
+            >
+              <Text variant="heading" sx={{ fontSize: 4, mb: 3 }}>
+                Hours
+              </Text>
+              <HoursComponent id={id} location={location} />
+            </Box>
+          ) : (
+            ''
+          )}
         </Box>
       </Flex>
     </Box>
