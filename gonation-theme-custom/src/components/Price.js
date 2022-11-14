5a420ff69757451f582874404176604b7eed85af
Gabriel Pereira <omami.thatsgood@gmail.com>
2022-11-14 12:32:32 -0500

    [skip ci] adding variant pricing

--- gonation-theme-custom/src/components/Price.js
+++ gonation-theme-custom/src/components/Price.js
@@ -3,17 +3,45 @@ import { jsx, Box, Flex, Text } from 'theme-ui';
 import React from 'react';
 
 const Price = ({ variants, withDollar, toSide }) => {
+  const variantsDetector = () => {
+    if(variants.length > 1){
+      return(
+        variants.map((item) => (
+          <span style={{padding: '.3rem'}}>{item.price}</span>
+        ))
+      )
+    }
+    if(variants.length){
+      return (
+        <span>{variants[0].price}</span>
+      )
+    }
+  }
+  const variantsDetectorTitle = () => {
+    if(variants.length > 1){
+      return(
+        variants.map((item) => (
+          <span style={{padding: '.3rem'}}>{item.label}</span>
+        ))
+      )
+    }
+    if(variants.length){
+      return (
+        <span>{variants[0].label}</span>
+      )
+    }
+  }
   return (
     <Box>
       <Text as='span' sx={{ fontSize: 1 }}>
         {variants.length ? variants[0].labelTitle : ''}
       </Text>
       <Text as='span' sx={{ fontSize: 1 }}>
-        {variants.length ? variants[0].label : ''}
+        {variants.length ? variantsDetectorTitle() : ''}
       </Text>
-      <Text sx={{ textAlign: 'right', fontSize: 1 }}>
+      <Text sx={{ display: 'flex', justifyContent: 'space-around', fontSize: 1 }}>
         {withDollar ? '$' : ''}
-        {variants.length ? variants[0].price : ''}
+        {variantsDetector()}
       </Text>
     </Box>
   );
