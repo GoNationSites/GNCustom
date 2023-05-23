cfcfe40a597f4ef60798222cc810b261ffb5dd47
Gabriel Pereira <omami.thatsgood@gmail.com>
2023-05-23 10:35:47 -0400

    adding script widget

--- gonation-theme-custom/src/components/Layout.js
+++ gonation-theme-custom/src/components/Layout.js
@@ -1,22 +1,22 @@
 /** @jsx jsx */
-import { jsx, Box, Flex, Text } from 'theme-ui';
-import React, { useEffect } from 'react';
-import slugify from 'slugify';
-import Navigation from './Navigation';
+import { jsx, Box, Flex, Text } from "theme-ui";
+import React, { useEffect } from "react";
+import slugify from "slugify";
+import Navigation from "./Navigation";
 
-import '../styles/animate.css';
-import Footer from './Footer';
-import cloudinaryOptimize from '../helpers/cloudinaryHelper';
-import 'react-image-lightbox/style.css';
-import './index.css';
-import SEO from './SEO';
-import OtherLocationsBox from '../components/OtherLocationsBox';
+import "../styles/animate.css";
+import Footer from "./Footer";
+import cloudinaryOptimize from "../helpers/cloudinaryHelper";
+import "react-image-lightbox/style.css";
+import "./index.css";
+import SEO from "./SEO";
+import OtherLocationsBox from "../components/OtherLocationsBox";
 
 const Layout = ({ children, routes, pageContext, pageTitle }) => {
   useEffect(() => {
-    const script = document.createElement('script');
+    const script = document.createElement("script");
 
-    script.src = 'https://cdn.lightwidget.com/widgets/lightwidget.js';
+    script.src = "https://cdn.lightwidget.com/widgets/lightwidget.js";
     script.async = true;
 
     document.body.appendChild(script);
@@ -26,6 +26,50 @@ const Layout = ({ children, routes, pageContext, pageTitle }) => {
     };
   });
 
+  useEffect(() => {
+    const script = document.createElement("script");
+    const location = slugify(pageContext.data.city, { lower: true });
+    if (location === "newtown") {
+      script.textContent = ` 
+      (function (w, d, s, o, f, js, fjs) {
+          w[o] = w[o] || function (opt) { w[o].q = w[o].q || opt };
+          (js = d.createElement(s)), (fjs = d.getElementsByTagName(s)[0]);
+          js.src = f;
+          js.async = 1;
+          if (fjs) { fjs.parentNode.insertBefore(js, fjs) } else { fjs = d.getElementsByTagName('head')[0]; fjs.appendChild(js) }
+        })(window, document, 'script', 'SliceWidgets', 'https://restaurant-widgets-integrations.slicelife.com/widget.js');
+        SliceWidgets({ shopUuid: '744b50ca-eb01-4707-9ad8-ecb4eddb827d' });`;
+    }
+    if (location === "ridgefield") {
+      script.textContent = `
+        (function (w, d, s, o, f, js, fjs) {
+            w[o] = w[o] || function (opt) { w[o].q = w[o].q || opt };
+            (js = d.createElement(s)), (fjs = d.getElementsByTagName(s)[0]);
+            js.src = f;
+            js.async = 1;
+            if (fjs) { fjs.parentNode.insertBefore(js, fjs) } else { fjs = d.getElementsByTagName('head')[0]; fjs.appendChild(js) }
+          })(window, document, 'script', 'SliceWidgets', 'https://restaurant-widgets-integrations.slicelife.com/widget.js');
+          SliceWidgets({ shopUuid: '7a5dab5b-a969-4f3c-9e13-f311471b5674' });
+      `;
+    }
+    if (location === "wilton") {
+      script.textContent = `
+      (function (w, d, s, o, f, js, fjs) {
+            w[o] = w[o] || function (opt) { w[o].q = w[o].q || opt };
+            (js = d.createElement(s)), (fjs = d.getElementsByTagName(s)[0]);
+            js.src = f;
+            js.async = 1;
+            if (fjs) { fjs.parentNode.insertBefore(js, fjs) } else { fjs = d.getElementsByTagName('head')[0]; fjs.appendChild(js) }
+          })(window, document, 'script', 'SliceWidgets', 'https://restaurant-widgets-integrations.slicelife.com/widget.js');
+          SliceWidgets({ shopUuid: 'cdffe44d-4980-4eb6-ac98-5989fd81048c' });
+      `;
+    }
+    document.body.appendChild(script);
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
   const footerBG = () =>
     cloudinaryOptimize(
       `https://res.cloudinary.com/gonation/image/upload/v1598471061/sites/red-rooster/footer-bg.jpg`,
@@ -33,55 +77,57 @@ const Layout = ({ children, routes, pageContext, pageTitle }) => {
     );
 
   const location = slugify(pageContext.data.city, { lower: true });
-
   const renderInstagramWidget = () => {
     switch (location) {
-      case 'wilton':
+      case "wilton":
         return (
           <iframe
-            src='https://cdn.lightwidget.com/widgets/22c8aff3d6515bcc9ba42e9b27337e03.html'
-            scrolling='no'
-            allowtransparency='true'
-            class='lightwidget-widget'
+            src="https://cdn.lightwidget.com/widgets/22c8aff3d6515bcc9ba42e9b27337e03.html"
+            scrolling="no"
+            allowtransparency="true"
+            class="lightwidget-widget"
             style={{
-              width: '100%',
+              width: "100%",
               border: 0,
-              overflow: 'hidden',
-            }}></iframe>
+              overflow: "hidden",
+            }}
+          ></iframe>
         );
-      case 'ridgefield':
+      case "ridgefield":
         return (
           <iframe
-            src='https://cdn.lightwidget.com/widgets/1e722e3325b75cd9b120646609569842.html'
-            scrolling='no'
-            allowtransparency='true'
-            class='lightwidget-widget'
+            src="https://cdn.lightwidget.com/widgets/1e722e3325b75cd9b120646609569842.html"
+            scrolling="no"
+            allowtransparency="true"
+            class="lightwidget-widget"
             style={{
-              width: '100%',
+              width: "100%",
               border: 0,
-              overflow: 'hidden',
-            }}></iframe>
+              overflow: "hidden",
+            }}
+          ></iframe>
         );
-      case 'newtown':
+      case "newtown":
         return (
           <iframe
-            src='https://cdn.lightwidget.com/widgets/834c2a22b71956d2a5c72ed80d7bdde0.html'
-            scrolling='no'
-            allowtransparency='true'
-            class='lightwidget-widget'
+            src="https://cdn.lightwidget.com/widgets/834c2a22b71956d2a5c72ed80d7bdde0.html"
+            scrolling="no"
+            allowtransparency="true"
+            class="lightwidget-widget"
             style={{
-              width: '100%',
+              width: "100%",
               border: 0,
-              overflow: 'hidden',
-            }}></iframe>
+              overflow: "hidden",
+            }}
+          ></iframe>
         );
       default:
-        return '';
+        return "";
     }
   };
 
   return (
-    <Box as='main' sx={{ marginRight: ['unset', '53px'] }}>
+    <Box as="main" sx={{ marginRight: ["unset", "53px"] }}>
       <SEO pageTitle={pageTitle} siteMetaData={pageContext.data} />
       <Navigation routes={routes} pageContext={pageContext} />
       {children}
@@ -91,14 +137,15 @@ const Layout = ({ children, routes, pageContext, pageTitle }) => {
       </Box>
       <Box>
         <Text
-          as='h4'
-          variant='title'
+          as="h4"
+          variant="title"
           sx={{
             fontSize: [3, 4, 6],
-            textAlign: 'center',
+            textAlign: "center",
             mb: [2, 3, 4],
-            fontFamily: 'heading',
-          }}>
+            fontFamily: "heading",
+          }}
+        >
           @{`redroosterpub${location}`}
         </Text>
       </Box>
